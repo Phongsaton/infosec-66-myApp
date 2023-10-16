2346b63e313cbe86a3a97edaa6f4a06f3e3c707f
Phongsaton <koonagarock@gmail.com>
2023-10-16 11:39:28 +0700

    decode data

--- src/api/contact/content-types/contact/lifecycles.js
+++ src/api/contact/content-types/contact/lifecycles.js
@@ -1,20 +1,21 @@
 module.exports = {
     async beforeCreate(event){
-        console.log('beforeSave ', event)
+        //console.log(event)
         event.params.data.mobile = btoa(event.params.data.mobile)
         event.params.data.card_id = btoa(event.params.data.card_id)
     },
 
     async afterFindOne(event){
-        console.log('afterFindOne ', event)
+        console.log(event)
+        if(!event.result) return
         event.result.mobile = atob(event.result.mobile)
         event.result.card_id = atob(event.result.card_id)
-    },
+     },
 
     async afterFindMany(event){
-        //await event.result.forEach(item => {
-            //item.mobile = atob(item.mobile)
-            //item.card_id = atob(item.card_id)
-        //});
-    }
+        await event.result.forEach(item => {
+            item.mobile = atob(item.mobile)
+             item.card_id = atob(item.card_id)
+         });
+     }
 }
